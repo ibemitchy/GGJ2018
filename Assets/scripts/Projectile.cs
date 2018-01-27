49894a2378ab9defd4e79da85b0a29a4d26ae239
JaF95 <jaffulljames@gmail.com>
2018-01-27 00:03:35 -0800

    Implemented projectiles
    Added Visual Studios specific file types to the ignore list
    Created Projectile.prefab
    Created Projectile.cs

--- Assets/scripts/Projectile.cs
+++ Assets/scripts/Projectile.cs
@@ -2,15 +2,93 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour {
+public class Projectile : MonoBehaviour
+{
+    /*=================*/
+    /*          MEMBERS             */
+    /*=================*/
+    /*              PUBLIC               */
+    [Tooltip("How long the projectile will be on the screen (in seconds)")]
+    public float lifespan;
+    [Tooltip("How fast the projectile will move")]
+    public float speed;
+    
+    /*             PRIVATE            */
+    //Timer for counting how many seconds before this projectile is destroyed
+    float lifeTimer;
+    //The 3D velocity of this projectile
+    Vector3 velocity;
 
-	// Use this for initialization
-	void Start () {
-		
+    /*=================*/
+    /*          METHODS            */
+    /*=================*/
+    /*              PUBLIC               */
+    //Returns the lifespan (in seconds) of this projectile
+    public float GetLifespan()
+    {
+        return lifespan;
+    }
+    //Returns the timer (in seconds) of this projectile
+    public float GetLifeTimer()
+    {
+        return lifeTimer;
+    }
+    //Returns the speed of this projectile
+    public float GetSpeed()
+    {
+        return speed;
+    }
+    //Sets the lifespan of the projectile to the given time (in seconds)
+    public void SetLifespan(float time)
+    {
+        lifespan = time;
+    }
+    //Sets the speed of the projectile
+    public void SetSpeed(float value)
+    {
+        speed = value;
+    }
+
+    /*             PRIVATE            */
+    //Method to move the projectile forward
+    void Move()
+    {
+        velocity = Vector3.forward * speed;
+        transform.position += velocity;
+    }
+    //Unity collision method
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("[Projectile.cs] " + gameObject.name + " hit " + collision.gameObject.name);
+
+        //Check the tag of the other object
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            //This projectile has hit a player
+        }
+        else if(collision.gameObject.CompareTag("Wall"))
+        {
+            //This projectile has hit a wall
+        }
+    }
+    //Unity initialization method
+    void Start ()
+    {
+        lifeTimer = lifespan;
+        velocity = Vector3.zero;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	//Unity update method
+	void Update ()
+    {
+        //Move the projectile
+        Move();
+
+        //Run the timer
+        lifeTimer -= Time.deltaTime;
+        if(lifeTimer <= 0.0f)
+        {
+            //If the timer is out, kill this projectile
+            DestroyImmediate(gameObject);
+        }
 	}
 }
