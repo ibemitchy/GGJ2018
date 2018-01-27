1805bbcb2829f14d91446d18195aa5494d8a07c1
RickyHui1997 <rickygordonhui@hotmail.com>
2018-01-26 23:44:48 -0800

    Player Movement added

---
+++ Assets/scripts/PlayerMovementControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementControl : MonoBehaviour {
+    public float speed;
+
+	// Use this for initialization
+    void Start () {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate () {
+        float horizontalMovement = Input.GetAxis("Horizontal") * speed;
+        float verticalMovement = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(horizontalMovement, 0, verticalMovement);
+        transform.Translate(movement);
+	}
+
+    //give a new speed for player movement.
+    //used for speed up boost 
+    public void SetSpeed(int newSpeed){
+        speed = newSpeed;
+    }
+
+}
