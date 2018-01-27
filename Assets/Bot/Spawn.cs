d22a1951075540fa6978855b146c1b2eb1e19cb0
Mobileheo <mobileheo@gmail.com>
2018-01-27 15:57:10 -0800

    NavMesh, NaveObstacle, BotObject

---
+++ Assets/Bot/Spawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject nagent;
+	public GameObject goalObject;
+
+	void start() 
+	{
+		Invoke("SpawnAgent", 2);
+	}
+
+	void spawnAgent() 
+	{
+		GameObject na = (GameObject) Instantiate(nagent, this.transform.position, Quaternion.identity);
+		na.GetComponent<BotMovement>().goal = goalObject.transform;
+		Invoke("SpawnAgent", Random.Range(2,5));
+	}
+}
