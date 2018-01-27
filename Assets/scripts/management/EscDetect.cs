9dda2063890fe845cc3e1be0329928d6d61ea2de
Forrest <lljsg123123@gmail.com>
2018-01-27 15:49:40 -0800

    Add UI manager and EscDetect

---
+++ Assets/scripts/management/EscDetect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscDetect : MonoBehaviour {
+
+    private GameObject panel;
+
+	// Use this for initialization
+	void Start () {
+        panel = GetComponent<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape)){
+            panel.gameObject.SetActive(true);
+        }
+	}
+}
