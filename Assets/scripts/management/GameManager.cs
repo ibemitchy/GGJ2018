4d9e4fb127e1cf93f77ae2b3093417badc634569
JaF95 <jaffulljames@gmail.com>
2018-01-27 16:54:28 -0800

    Management Setup
    Setup the Player manager to hold a reference to all players in the scene
    Setup the win condition check in the Game manager

--- Assets/scripts/management/GameManager.cs
+++ Assets/scripts/management/GameManager.cs
@@ -1,17 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance { get; private set; }
 
+    private bool gameOver;
+    private bool possibleWinner;
+    private List<GameObject> spawnPoints;
+
+    public static bool GetGameOverFlag()
+    {
+        return instance.gameOver;
+    }
 	public static GameManager GetInstance()
     {
         return instance;
     }
+    public static List<GameObject> GetSpawnPoints()
+    {
+        return instance.spawnPoints;
+    }
 
-    private void Awake()
+    void Awake()
     {
         if (instance == null)
         {
@@ -22,6 +35,32 @@ public class GameManager : MonoBehaviour
         {
             DestroyImmediate(gameObject);
             return;
+        }
+    }
+    void Start()
+    {
+        gameOver = false;
+        possibleWinner = false;
+    }
+    void Update()
+    {
+        //Go through the list of players and see who's alive still
+        foreach(KeyValuePair<int, GameObject> player in PlayerManager.GetPlayerList())
+        {
+            if(!possibleWinner)
+            {
+                possibleWinner = true;
+            }
+            else
+            {
+                possibleWinner = false;
+                break;
+            }
+        }
+
+        if(possibleWinner)
+        {
+            gameOver = true;
         }
     }
 }
