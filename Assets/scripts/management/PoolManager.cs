e597414522e65b8ff593496e97787e1760fb57e7
JaF95 <jaffulljames@gmail.com>
2018-01-28 13:25:33 -0800

    Updated PoolManager
    Pool manager will now pool the impact effects
    Added the trap door model and animations
    Added the spit particle model

--- Assets/scripts/management/PoolManager.cs
+++ Assets/scripts/management/PoolManager.cs
@@ -6,14 +6,34 @@ public class PoolManager : MonoBehaviour
 {
     public static PoolManager instance { get; private set; }
 
+    [Tooltip("The impact effect that should spawn when the projectile collides with something")]
+    public GameObject impactEffectPrefab;
     [Tooltip("The projectile object that should spawn when the player shoots")]
     public GameObject projectilePrefab;
+    [Tooltip("The maximum number of impact effects allowed in this scene")]
+    public int maxNumImpactEffects;
     [Tooltip("The maximum number of projectiles allowed in this scene")]
     public int maxNumProjectiles;
 
+    //The pool of all impact effects in the scene
+    private GameObject[] impactEffectPool;
     //The pool of all projectiles in the scene
     private GameObject[] projectilePool;
 
+    //A method to return the next impact effect from the pool that is not in use
+    public static GameObject GetImpactEffect()
+    {
+        foreach(GameObject impactEffect in instance.impactEffectPool)
+        {
+            if(!impactEffect.activeSelf)
+            {
+                return impactEffect;
+            }
+        }
+
+        Debug.LogError("[PoolManager.cs] All impact effects are in use!");
+        return null;
+    }
     //A method to return the next projectile object from the pool that is not in use
     public static GameObject GetProjectile()
     {
@@ -28,10 +48,20 @@ public class PoolManager : MonoBehaviour
         Debug.LogError("[PoolManager.cs] All projectiles are in use!");
         return null;
     }
+    //A method to return the impact effect pool
+    public static GameObject[] GetImpactEffectPool()
+    {
+        return instance.impactEffectPool;
+    }
     //A method to return the projectile pool
     public static GameObject[] GetProjectilePool()
     {
         return instance.projectilePool;
+    }
+    //A method to return the max number of impact effects allowed in this scene
+    public static int GetMaxNumImpactEffects()
+    {
+        return instance.maxNumImpactEffects;
     }
     //A method to return the max number of projectiles allowed in this scene
     public static int GetMaxNumProjectiles()
@@ -72,6 +102,23 @@ public class PoolManager : MonoBehaviour
             projectilePool[p] = Instantiate(projectilePrefab, Vector3.zero, Quaternion.identity);
             projectilePool[p].transform.parent = parent.transform;
             projectilePool[p].SetActive(false);
+        }
+
+        //initialize the impact effect pool
+        impactEffectPool = new GameObject[maxNumImpactEffects];
+
+        //Create a parent object for the impact effects
+        GameObject impactParent = new GameObject();
+        impactParent.name = "Impact Effect Pool";
+
+        //Create each impact effect and set them to being inactive
+        for(int i = 0; i < maxNumImpactEffects; ++i)
+        {
+            impactEffectPool[i] = Instantiate(impactEffectPrefab, Vector3.zero, Quaternion.identity);
+            impactEffectPool[i].GetComponent<ParticleSystem>().Stop();
+
+            impactEffectPool[i].transform.parent = impactParent.transform;
+            impactEffectPool[i].SetActive(false);
         }
     }
 }
